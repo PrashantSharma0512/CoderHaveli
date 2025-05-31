ac25431163acfc9f74ceb5cf8aad6ccf1d66f655
PrashantSharma0512 <prashantsharma0512@gmail.com>
2025-05-31 22:35:11 +0530

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://coderhaveli-com.onrender.com', // Replace with your backend server URL
+        target: 'https://coderhaveli-com.onrender.com', 
         changeOrigin: true,
         secure: false,
         ws:true,
